682d972d0a1b73034307605cf5ece829d924d7e3
Hiroyuki Hosokawa <simamaru@gmail.com>
2024-01-03 16:19:14 +0900

    first commit

---
+++ theme.config.tsx
@@ -0,0 +1,10 @@
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  logo: <span>Nextra Sample</span>,
+  project: {
+    link: 'http://localhost:3000',
+  }
+}
+
+export default config
